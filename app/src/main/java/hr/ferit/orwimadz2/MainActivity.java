5e133b921be0f4f4fe8aa5d4b3927a650c21baac
Mimi <mislav.kostiac@gmail.com>
2020-11-22 14:18:36 +0100

    1st working test version

--- app/src/main/java/hr/ferit/orwimadz2/MainActivity.java
+++ app/src/main/java/hr/ferit/orwimadz2/MainActivity.java
@@ -1,14 +1,46 @@
 package hr.ferit.orwimadz2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAdd;
+    private EditText etName;
+    private RecyclerView recyclerView;
+    private RecyclerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupRecycler();
+        setupRecyclerData();
     }
+
+    private void setupRecycler() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setupRecyclerData() {
+        List<String> names = new ArrayList<>();
+        names.add("Ezio");
+        names.add("Conor");
+        names.add("Eivor");
+
+        adapter.addNames(names);
+    }
+
 }
--- app/src/main/java/hr/ferit/orwimadz2/RecyclerAdapter.java
+++ app/src/main/java/hr/ferit/orwimadz2/RecyclerAdapter.java
@@ -1,4 +1,38 @@
 package hr.ferit.orwimadz2;
 
-public class RecyclerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+    private List<String> names = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View nameView = LayoutInflater.from(parent.getContext()).inflate(R.layout.name_card, parent, false);
+        return new ViewHolder(nameView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setName(names.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.size();
+    }
+
+    public void addNames(List<String> names){
+        this.names.addAll(names);
+        notifyDataSetChanged();
+    }
 }
